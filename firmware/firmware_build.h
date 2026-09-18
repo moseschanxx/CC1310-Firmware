9da71219c55cf0ca9ad7ea095337a28498d72407
Eric <eric@build.ai>
2026-09-18 13:40:48 +0800

    Bump firmware version to 0.2.1

--- firmware/firmware_build.h
+++ firmware/firmware_build.h
@@ -3,6 +3,6 @@
 
 /* Single release version source. firmware/build.sh encodes major.minor.patch
  * as 0x00MMmmpp in the uint32 OTA header. */
-#define FIRMWARE_VERSION "0.2.0"
+#define FIRMWARE_VERSION "0.2.1"
 
 #endif
